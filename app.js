be008c0dee27ec1ceb5291b5106d7e486cb3ca20
Dimar16 <dianam.pedroza@gmail.com>
2022-08-17 13:04:43 -0500

    arreglar public

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express ();
 
 const path = require ("path");
 
-app.use (express.static ("public"));
+app.use (express.static ("./public"));
 
 app.listen (process.env.PORT || 3000, ()=> console.log ("The server runs in 3000 port"));
 
